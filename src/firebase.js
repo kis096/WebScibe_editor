4ee4299190514aa1ca3c768568c264b45949c7d5
Kishan <ranakishan3050@gmail.com>
2024-09-02 15:43:02 +0530

    Adding firebase for hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUpY06HPCVRjzWmfnvN_mt8PjJRWhIMrk",
+  authDomain: "webscribe-4ba5c.firebaseapp.com",
+  projectId: "webscribe-4ba5c",
+  storageBucket: "webscribe-4ba5c.appspot.com",
+  messagingSenderId: "679080149722",
+  appId: "1:679080149722:web:7361dec01a097159081be3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
